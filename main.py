406b3e34968d1813b4fe5f9d226d9b6e6d08f743
Bernardo Miranda <bvm@poli.ufrj.br>
2019-11-28 14:39:14 +0100

    Addition of Buttons for adding songs and labels for sections of the window. Still without functionalities

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 # Main python file for creating the GUI using the kv file switcher.kv
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+
+class MainWindow(Screen):
+    filename = ObjectProperty(None)
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("switcher.kv")
+sm = WindowManager()
+
+screens = [MainWindow(name="main")]
+
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "main"
+
+class MyMainApp(App):
+    def build(self):
+        return sm
+
+if __name__ == "__main__":
+    MyMainApp().run()
