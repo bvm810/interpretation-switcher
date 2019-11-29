d8be7aaa676c1aa57657aac11126394725b99adf
Bernardo Miranda <bvm@poli.ufrj.br>
2019-11-29 13:35:35 +0100

    SVG container for reading sheet music in this format

--- main.py
+++ main.py
@@ -2,25 +2,41 @@
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.graphics.svg import Svg
+from kivy.uix.scatter import Scatter
 
 
-root = Builder.load_file("./switcher.kv") # Loads GUI background and basic displays
+Builder.load_file("./switcher.kv")
 
-# Creates a box layout on the left-hand side to house the song progress bars
-songbox = BoxLayout(
-    orientation = "vertical",
-    size_hint=(0.3, 0.8),
-    pos_hint={'x': 0.0, 'y': 0.2})
+class BackgroundWidget(FloatLayout):
+    pass
 
-# Creates a box on the right hand side to house the music sheet image
-sheetbox = BoxLayout(
-    orientation = "vertical",
-    size_hint=(0.7, 0.8),
-    pos_hint={'x': 0.3, 'y': 0.2})
+class SongboxWidget(BoxLayout):
+    pass
+
+class SheetboxWidget(BoxLayout):
+    pass
+
+class SvgWidget(Scatter):
+
+    def __init__(self, filename, **kwargs):
+        super(SvgWidget, self).__init__(**kwargs)
+        with self.canvas:
+            svg = Svg(filename)
+        self.size = svg.width, svg.height
+
+
+#BACKEND MAGIC
+root = BackgroundWidget()
+songbox = SongboxWidget(size_hint= (0.3, 0.8),pos_hint={"x":0.0, "y": 0.2})
+sheetbox = SheetboxWidget(size_hint= (0.7, 0.8),pos_hint={"x":0.3, "y": 0.2})
+
+
 
 root.add_widget(songbox)
+root.add_widget(sheetbox)
 
 class MainApp(App):
 
