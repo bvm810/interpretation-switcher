8b7a3c9fa9added544a443ffe3fc5d137ed3bb74
Bernardo Miranda <bvm@poli.ufrj.br>
2020-02-11 16:58:18 +0100

    Version 2 of audioplayer

--- alignment/player.py
+++ alignment/player.py
@@ -1,48 +1,27 @@
-import pyaudio
-import wave
+from pygame import mixer as mix
 import time
-from pynput import keyboard
 
-def audioplayer(filename):
-    paused = False    # nonlocal to track if the audio is paused
+def init_audioplayer(filelist, frequency = 44100, size = -16, channels = 2, buffer = 512):
+    """
+    Function for initializing pygame's mixer module with the desired wav files in its channels.
 
-    def pause(key):
-        nonlocal paused
-        print (key)
-        if key == keyboard.Key.space:
-            if stream.is_stopped():     # time to play audio
-                print ('play pressed')
-                stream.start_stream()
-                paused = False
+    :param filelist: List of strings with filenames of audiofiles to be aligned
+    :return channel_list: List of Channel objects tuples to be manipulated with play/pause
+    """
+    mix.init(frequency=frequency, size=size, channels=channels, buffer=buffer)
+    channel_list = []
+    for name in filelist:
+        sound = mix.Sound(name)
+        channel = mix.Channel(filelist.index(name))
+        channel.play(sound)
+        channel.pause()
+        channel_list.append(channel)
+    return channel_list
 
-            elif stream.is_active():   # time to pause audio
-                print ('pause pressed')
-                stream.stop_stream()
-                paused = True
-
-    wf = wave.open('/Users/bernardo/Documents/Projetos em Andamento/Projet Long S7+S8/interpretation-switcher/alignment/'+filename, 'rb')
-    p = pyaudio.PyAudio()
-
-    def callback(in_data, frame_count, time_info, status):
-        data = wf.readframes(frame_count)
-        return (data, pyaudio.paContinue)
-
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True,
-                stream_callback=callback)
-
-    stream.start_stream()
-    while stream.is_active() or paused==True:
-        with keyboard.Listener(on_press=pause) as listener:
-            listener.join()
-        time.sleep(0.1)
-
-    stream.stop_stream()
-    stream.close()
-    wf.close()
-
-    p.terminate()
-
-song1 = audioplayer('Chopin Prelude Op. 28 No. 7 N. Freire.wav')
+# Test
+filenames = ['Chopin Prelude Op. 28 No. 7 M. Pollini.wav', 'Chopin Prelude Op. 28 No. 7 MIDI.wav', 'Chopin Prelude Op. 28 No. 7 N. Freire.wav']
+channel_list = init_audioplayer(filenames)
+for channel in channel_list:
+    channel.unpause()
+    time.sleep(10)
+    channel.pause()
