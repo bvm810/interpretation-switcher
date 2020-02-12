6650e45c5677e2812184c50783b3f9fdd72276a0
Bernardo Miranda <bvm@poli.ufrj.br>
2020-02-12 16:59:47 +0100

    Try with scheduled events

--- alignment/player.py
+++ alignment/player.py
@@ -1,12 +1,14 @@
 import pyaudio
 import wave
 import time
+import sched
 
 class AudioPlayer():
 
     def __init__(self, filename):
         self.is_playing = False
         self.currentSample = 0
+        self.chunk = 1024
         self.file = wave.open(filename, 'rb')
 
         self.p = pyaudio.PyAudio()
@@ -14,33 +16,22 @@ class AudioPlayer():
         self.stream = self.p.open(format=self.p.get_format_from_width(self.file.getsampwidth()),
                 channels=self.file.getnchannels(),
                 rate=self.file.getframerate(),
-                output=True,
-                stream_callback=self.callback)
+                output=True)
 
     def play(self):
-        self.stream.start_stream()
         self.is_playing = True
-
-        while self.stream.is_active():
-            time.sleep(0.1)
+        self.callback()
 
     def pause(self):
-        self.stream.stop_stream()
         self.is_playing = False
 
-        # self.stream.close()
-        # self.file.close()
-        #
-        # self.p.terminate()
+    def callback(self):
+        data = self.file.readframes(self.chunk)
 
+        if len(data) <= 0:
+            self.stream.stop_stream()
+            self.stream.close()
+            self.p.terminate()
 
-    def callback(self, in_data, frame_count, time_info, status):
-        data = self.file.readframes(frame_count)
-        return (data, pyaudio.paContinue)
-
-a = AudioPlayer('Chopin Prelude Op. 28 No. 7 N. Freire.wav')
-a.play()
-time.sleep(5)
-a.pause()
-time.sleep(2)
-a.play()
+        if self.is_playing == True:
+            self.stream.write(data)
